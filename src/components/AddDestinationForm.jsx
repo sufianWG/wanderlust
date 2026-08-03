1ed12550501a4fa9d78291131c666d5fd7b51827
sufianWG <webgurumrmbd@gmail.com>
2026-08-03 14:31:45 +0600

    added add-destination form & added data posting functionalties to the backend

--- src/components/AddDestinationForm.jsx
+++ src/components/AddDestinationForm.jsx
@@ -4,11 +4,24 @@ import { FieldError, Input, Label, TextField, Select, ListBox, TextArea, Button,
 import React from 'react';
 
 const AddDestinationForm = () => {
-    const onSubmit = (e) => {
+    const onSubmit = async(e) => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget)
         const destination = Object.fromEntries(formData.entries());
-        console.log(destination);
+        // console.log(destination);
+
+        const res = await fetch('http://localhost:5260/destination', {
+            method: "POST",
+            headers: {
+                "content-type" : "application/json"
+            },
+            body: JSON.stringify(destination)
+        })
+
+        const data = await res.json();
+        console.log(data);
+
+
     }
     return (
         <div className='p-5 max-w-3xl mx-auto'>
