839c20a7ef46bbfa2232ec495d3aa008258c4c44
sufianWG <webgurumrmbd@gmail.com>
2026-08-03 12:06:53 +0600

    project setup done & added navBar, banner & footer

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="bg-white py-3">
+            <nav className="flex items-center justify-between max-w-7xl mx-auto">
+                <ul className="flex gap-3">
+                    <li>
+                        <Link href={"/"}>Home</Link>
+                    </li>
+                    <li>
+                        <Link href={"/destinations"}>Destinations</Link>
+                    </li>
+                    <li>
+                        <Link href={"/my-bookings"}>My Bookings</Link>
+                    </li>
+
+                    <li>
+                        <Link href={"/add-destination"}>Add Destination</Link>
+                    </li>
+                </ul>
+
+                <div>
+                    <Image
+                        src={"/assets/Wanderlast.png"}
+                        height={150}
+                        width={150}
+                        alt="logo"
+                    />
+                </div>
+
+                <ul className="flex items-center gap-3">
+                    <li>
+                        <Link href={"/profile"}>Profile</Link>
+                    </li>
+                    <li>
+                        <Link href={"/login"}>Login</Link>
+                    </li>
+                    <li>
+                        <Link href={"/signup"}>Sign Up</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
